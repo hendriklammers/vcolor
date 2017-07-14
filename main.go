4beac259a39f9e0a52ba92164cae314c4c7d7518
Hendrik Lammers <lammers@gmail.com>
2017-07-14 17:52:54 +0200

    Allow rgba colors

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 )
 
+const regex = "(?i)^#?([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})?$"
+
 func main() {
 	args := os.Args
 
@@ -27,25 +29,25 @@ func main() {
 
 // ParseColor takes hex color string and converts it to a string in vec3 format
 func ParseColor(hex string) (string, error) {
-	re := regexp.MustCompile("(?i)^#?([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$")
+	re := regexp.MustCompile(regex)
 
 	if !re.MatchString(hex) {
 		return "", errors.New("Not a valid hex color")
 	}
 
-	values := make([]string, 3)
-	for index, match := range re.FindStringSubmatch(hex)[1:] {
-		num, err := strconv.ParseInt(match, 16, 16)
-		if err != nil {
-			return "", err
+	var values []string
+	for _, match := range re.FindStringSubmatch(hex)[1:] {
+		if match != "" {
+			num, err := strconv.ParseInt(match, 16, 16)
+			if err != nil {
+				return "", err
+			}
+			val := fmt.Sprintf("%.3f", float32(num)/255)
+			values = append(values, stripZeros(val))
 		}
-
-		val := fmt.Sprintf("%.3f", float32(num)/255)
-
-		values[index] = stripZeros(val)
 	}
 
-	return fmt.Sprintf("vec3(%v)", strings.Join(values, ", ")), nil
+	return fmt.Sprintf("vec%v(%v)", len(values), strings.Join(values, ", ")), nil
 }
 
 func stripZeros(str string) string {
